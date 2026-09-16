5e18ced3b5fae0a1972f8e25484b66907a08d3e4
Stefano Calderone <steve74c@libero.it>
2026-09-16 22:55:41 +0200

    rimuovo copia core da dentro app

--- app/core/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types';
-export * from './gioco';
-export { valutaTentativo } from './valutaTentativo';
-export { normalizzaParola } from './normalizza';
-export { CONFIG_DEFAULT } from './config';
-export { parolaValida, isValidWordStub } from './dizionario';
-export { PAROLE_DEV, pescaParolaCasuale } from './paroleDev';
